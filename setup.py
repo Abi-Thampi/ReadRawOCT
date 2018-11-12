2b4c24a395f5bc2d059aef046f32f71c360cfab3
Hitchman <Sam.Hitchman@agresearch.co.nz>
2018-11-13 08:50:13 +1300

    Fixed install

--- setup.py
+++ setup.py
@@ -5,5 +5,4 @@ setup(
     version='0.1dev',
     packages=['ReadRawOCT',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
 )
